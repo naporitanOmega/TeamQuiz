1b40ab6b0eaeff453b2294c0403810eb5f79d216
Kaishu <fkaishu0812@gmail.com>
2020-02-03 16:35:27 +0900

    キー入力で文字表示

--- title.js
+++ title.js
@@ -1,18 +1,43 @@
-var word = document.querySelector('#word');
+﻿var word = document.querySelector('#word');
 
 addEventListener("keydown", keydownfunc, false);
 
 function keydownfunc(event) {
-
-    word.innerHTML = String.fromCharCode(event.keyCode);
-    b();
+    drawWord(String.fromCharCode(event.keyCode));
 }
 
-var x = 0;
-function b() {
-    x++;
-    document.all.word.style.fontSize = x + 'px';
-    if (x < 45) {
-        setTimeout("b()", 50);
+/* 文字埋め */
+var mojiume = new Object();
+
+mojiume.count = 0;
+mojiume.limit = 1000;
+
+mojiume.set = function () {
+    //var span = document.body.get;
+
+    //var i = 0;
+    //for (i = 0; i < spans.length; i++) {
+    //    spans[i].style.opacity -= 0.1;
+    //}
+
+    //alert(span.length);
+};
+
+function drawWord(string) {
+    const div1 = document.getElementById("div1");
+
+    var span = document.createElement("span");
+    span.className = "mojiume";
+    span.style.left = (Math.random() * 100) + "%";
+    span.style.top = (Math.random() * 100) + "%";
+    span.style.transform =
+        "rotate(" + ((Math.random() * 120) - 60) + "deg)";
+    span.textContent = string;
+
+    document.body.appendChild(span);
+    if (document.querySelectorAll(".mojiume").length >= mojiume.limit) {
+        clearInterval(mojiume.tmr);
     }
-}
+};
+
+mojiume.tmr = setInterval(mojiume.set, 5000);
